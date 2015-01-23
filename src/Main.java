2aa76d280f22101db3e20a247a362ba320de3de1
Tim Cox <tim_cox@apple.com>
2015-01-23 20:38:50 +0000

    again

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+        List<DropboxFile> all = new ArrayList<DropboxFile>();
         for (int i = 0; i < 100; i++) {
-            System.out.println(new TempFile());
+            all.add(new TempFile());
         }
+
+        String json = makeJson(all);
+        System.out.println(json);
+    }
+
+    private static String makeJson(List<DropboxFile> all) {
+        Gson gson = new Gson();
+        return "";
     }
 
 }
