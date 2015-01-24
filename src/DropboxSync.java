9ef9729fb184796b2c2035b575447a56e07f500f
Marvin Hoffmann <marhoffm@adobe.com>
2015-01-24 11:03:37 +0000

    - implemented upload file method - more refactoring and exception handling

--- src/DropboxSync.java
+++ src/DropboxSync.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.security.MessageDigest;
@@ -21,40 +21,21 @@ import com.dropbox.core.DbxEntry;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.DbxWebAuthNoRedirect;
+import com.dropbox.core.DbxWriteMode;
 
 
-public class DropboxSync {
+public class DropboxSync implements IDropboxUpload {
 
 	final static String APP_KEY = "5qrvnudv5gltoy8";
 	final static String APP_SECRET = "ystltok87pzc6ue";
 	final static String ACCESS_TOKEN = "";
 
 	static String lastDeltaCursor = "";
+	static DbxClient client = null;
 
-	public static void main(final String[] args) throws IOException, DbxException, NoSuchAlgorithmException {
+	public static void main(final String[] args) throws DbxException {
 
-		final DbxAppInfo appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
-
-		final DbxRequestConfig config = new DbxRequestConfig(
-				"ContentAnalyzer/1.0", Locale.getDefault().toString());
-		String accessToken = ACCESS_TOKEN;
-
-		if (accessToken.isEmpty())
-		{
-			final DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
-
-			final String authorizeUrl = webAuth.start();
-			System.out.println("1. Go to: " + authorizeUrl);
-			System.out.println("2. Click \"Allow\" (you might have to log in first)");
-			System.out.println("3. Copy the authorization code.");
-			final String code = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
-
-			final DbxAuthFinish authFinish = webAuth.finish(code);
-			accessToken = authFinish.accessToken;
-			System.out.println(accessToken);
-		}
-
-		final DbxClient client = new DbxClient(config, accessToken);
+		final DbxClient client = getDbxClient();
 
 		final DbxAccountInfo accountInfo = getAccountInfo(client);
 		System.out.println("Linked account: " + accountInfo.displayName);
@@ -63,8 +44,46 @@ public class DropboxSync {
 		System.out.println("shared: " + accountInfo.quota.shared);
 		System.out.println("total: " + accountInfo.quota.total);
 
-		getFileList(client);
-		getDelta(client);
+		getFileList();
+		getDelta();
+	}
+
+	private static DbxClient getDbxClient() throws DbxException {
+
+		if (client == null)
+		{
+			final DbxAppInfo appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
+
+			final DbxRequestConfig config = new DbxRequestConfig(
+					"ContentAnalyzer/1.0", Locale.getDefault().toString());
+			String accessToken = ACCESS_TOKEN;
+
+			if (accessToken.isEmpty())
+			{
+				final DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
+
+				final String authorizeUrl = webAuth.start();
+				System.out.println("1. Go to: " + authorizeUrl);
+				System.out.println("2. Click \"Allow\" (you might have to log in first)");
+				System.out.println("3. Copy the authorization code.");
+				String code = "";
+
+				try {
+					code = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
+				} catch (IOException e) {
+					System.out.println("Error reading user input:");
+					e.printStackTrace();
+				}
+
+				final DbxAuthFinish authFinish = webAuth.finish(code);
+				accessToken = authFinish.accessToken;
+				System.out.println(accessToken);
+			}
+
+			client = new DbxClient(config, accessToken);
+		}
+
+		return client;
 	}
 
 	private static DbxAccountInfo getAccountInfo(final DbxClient client) throws DbxException {
@@ -73,19 +92,18 @@ public class DropboxSync {
 		return accountInfo;
 	}
 
-	private static void getFileList(final DbxClient client)
-			throws DbxException, IOException, NoSuchAlgorithmException {
+	private static void getFileList() throws DbxException {
 
-		final List<IDropboxFile> allFilesInDropbox = traverseHierarchy(client, "/");
+		final List<IDropboxFile> allFilesInDropbox = traverseHierarchy("/");
 	}
 
-	private static DbxDelta<DbxEntry> getDelta(final DbxClient client) throws DbxException {
-		final DbxDelta<DbxEntry> delta = client.getDelta(lastDeltaCursor.isEmpty() ? null : lastDeltaCursor);
+	private static DbxDelta<DbxEntry> getDelta() throws DbxException {
+		final DbxDelta<DbxEntry> delta = getDbxClient().getDelta(lastDeltaCursor.isEmpty() ? null : lastDeltaCursor);
 		lastDeltaCursor = delta.cursor;
 
 		if (delta.hasMore)
 		{
-			getDelta(client);
+			getDelta();
 		}
 
 		System.out.println("delta result:");
@@ -94,12 +112,12 @@ public class DropboxSync {
 		return delta;
 	}
 
-	private static List<IDropboxFile> traverseHierarchy(final DbxClient client, final String parentPath)
-			throws DbxException, IOException, NoSuchAlgorithmException {
+	private static List<IDropboxFile> traverseHierarchy(final String parentPath)
+			throws DbxException {
 
 		final List<IDropboxFile> files = new ArrayList<IDropboxFile>();
 
-		final DbxEntry.WithChildren listing = client.getMetadataWithChildren(parentPath);
+		final DbxEntry.WithChildren listing = getDbxClient().getMetadataWithChildren(parentPath);
 
 		for (final DbxEntry child : listing.children) {
 
@@ -107,13 +125,13 @@ public class DropboxSync {
 				final DbxEntry.File dropboxFile = child.asFile();
 				final DropboxFile file = new DropboxFile(dropboxFile);
 
-				final File tempFile = downloadFile(client, dropboxFile, file);
+				final File tempFile = downloadFile(getDbxClient(), dropboxFile, file);
 				file.setHash(hashFile(tempFile));
 
 				files.add(file);
 			}
 			else if (child.isFolder()) {
-				files.addAll(traverseHierarchy(client, child.asFolder().path));
+				files.addAll(traverseHierarchy(child.asFolder().path));
 			}
 		}
 
@@ -121,31 +139,54 @@ public class DropboxSync {
 	}
 
 	private static File downloadFile(final DbxClient client, final DbxEntry.File dropboxFile, final DropboxFile file)
-			throws IOException, FileNotFoundException, DbxException {
+			throws DbxException {
 
-		final File tempFile = File.createTempFile(UUID.randomUUID().toString(), file.filename());
-		final FileOutputStream target = new FileOutputStream(tempFile);
+		File tempFile = null;
+
 		try {
-			client.getFile(dropboxFile.path, dropboxFile.rev, target);
+			tempFile = File.createTempFile(UUID.randomUUID().toString(), file.filename());
+			final FileOutputStream target = new FileOutputStream(tempFile);
+
+			try {
+
+				client.getFile(dropboxFile.path, dropboxFile.rev, target);
+			}
+			finally {
+				target.close();
+			}
+		} catch (IOException e) {
+			System.out.println("Error creating temp file:");
+			e.printStackTrace();
 		}
-		finally {
-			target.close();
-		}
+
 		return tempFile;
 	}
 
-	private static String hashFile(final File tempFile)
-			throws NoSuchAlgorithmException, IOException {
+	private static String hashFile(final File tempFile) {
 
 		final String myHash = "MD5";
-		final MessageDigest complete = MessageDigest.getInstance(myHash);
-
-		final byte[] b = complete.digest(Files.readAllBytes(tempFile.toPath()));
+		MessageDigest complete;
 		String result = "";
-		for (int i=0; i < b.length; i++) {
-			result +=
-					Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+
+		try {
+			complete = MessageDigest.getInstance(myHash);
+
+			final byte[] b = complete.digest(Files.readAllBytes(tempFile.toPath()));
+
+			for (int i=0; i < b.length; i++) {
+				result +=
+						Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+			}
+		} catch (NoSuchAlgorithmException | IOException e) {
+			System.out.println("Error generating file hash:");
+			e.printStackTrace();
 		}
+
 		return result;
+	}
+
+	@Override
+	public void upload(String filenameIncludingPath, long size, InputStream data) throws DbxException, IOException {
+		getDbxClient().uploadFile(filenameIncludingPath, DbxWriteMode.force(), size, data);
 	}
 }
--- src/IDropboxUpload.java
+++ src/IDropboxUpload.java
@@ -1,9 +1,11 @@
-import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+
+import com.dropbox.core.DbxException;
 
 
 public interface IDropboxUpload {
 
-	void upload(String filenameIncludingPath, long size, InputStream data);
+	void upload(String filenameIncludingPath, long size, InputStream data) throws DbxException, IOException;
 
 }
